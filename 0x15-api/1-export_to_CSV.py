e8a127994477a9ca313e2b25d1c7bf02153a9fae
Pixel <piixel.1878@gmail.com>
2024-05-20 22:39:48 +0300

    test

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""For a given employee ID, returns information about
+their TODO list progress"""
+
+import requests
+import sys
+import csv
+
+if __name__ == "__main__":
+
+    userId = sys.argv[1]
+    user = requests.get("https://jsonplaceholder.typicode.com/users/{}"
+                        .format(userId))
+
+    name = user.json().get('name')
+
+    todos = requests.get('https://jsonplaceholder.typicode.com/todos')
+
+    file_name = userId + ".csv"
+    with open(file_name, 'w') as file:
+        writer = csv.writer(file, delimiter=',', quoting=csv.QUOTE_ALL)
+
+    for task in todos.json():
+        if task.get('userId') == int(userId):
+            writer.writerow([userId, name, task.get('completed'),
+                             task.get('title')])
