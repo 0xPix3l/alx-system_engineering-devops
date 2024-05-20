15255dcb1fd44235b5670d35d0cac66e48f780c3
Pixel <piixel.1878@gmail.com>
2024-05-20 22:34:39 +0300

    test

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,30 +1,30 @@
 #!/usr/bin/python3
-"""
-return info about info from a given ID
-"""
+"""For a given employee ID, returns information about
+their TODO list progress"""
 
 import requests
 import sys
 
 if __name__ == "__main__":
-    user_ID = sys.argv[1]
-    user = requests.get(
-        f'https://jsonplaceholder.typicode.com/users/{user_ID}')
+
+    userId = sys.argv[1]
+    user = requests.get("https://jsonplaceholder.typicode.com/users/{}"
+                        .format(userId))
 
     name = user.json().get('name')
 
-    todos = requests.get(f'https://jsonplaceholder.typicode.com/todos')
-
-    tasks = 0
-    done = 0
+    todos = requests.get('https://jsonplaceholder.typicode.com/todos')
+    totalTasks = 0
+    completed = 0
 
     for task in todos.json():
-        if task.get('userId') == int(user_ID):
-            tasks += 1
+        if task.get('userId') == int(userId):
+            totalTasks += 1
             if task.get('completed'):
-                done += 1
+                completed += 1
 
-    print(f"Employee {name} is done with tasks({done}/{tasks}):")
+    print('Employee {} is done with tasks({}/{}):'
+          .format(name, completed, totalTasks))
 
     print('\n'.join(["\t " + task.get('title') for task in todos.json()
-          if task.get('userId') == int(user_ID) and task.get('completed')]))
+          if task.get('userId') == int(userId) and task.get('completed')]))
